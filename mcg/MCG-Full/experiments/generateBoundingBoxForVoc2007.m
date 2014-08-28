1e418fdea9b0323cf1a9ce2c34cbe402ff258030
Harsh Agrawal <h.agrawal092@gmail.com>
2014-08-28 03:44:28 -0400

    Adding MCG Files

---
+++ mcg/MCG-Full/experiments/generateBoundingBoxForVoc2007.m
@@ -0,0 +1,28 @@
+function  generateProposalsForImages(range)
+cd('/home/gneelima/work/code/objectProposals/mcg/MCG-Full/');
+addpath(genpath(pwd));
+
+build;
+install;
+
+config=createConfig();
+
+imageLoc=config.path.input;
+outputLoc=config.path.output;
+%extension inclues .
+imageExt=config.param.imageExt;
+images=dir([imageLoc '*' imageExt]);
+
+for i=1:4952
+        imName=images(i).name;
+        image=imread([imageLoc imName]);
+        
+	if(~size(image,3)==3)
+		image=repmat(image,[1 1 3]);
+	end
+    %candidates stores bounding boxes too..
+	[candidates,scores]= im2mcg(image,'accurate');
+    proposalFileName=strrep(imName,imageExt,'.mat');
+    save([outputLoc proposalFileName],'candidates','scores');
+end
+
