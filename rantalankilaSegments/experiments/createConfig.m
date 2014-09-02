766f896f598e84c75e2baf7ebbcce921e914f491
Harsh Agrawal <h.agrawal092@gmail.com>
2014-09-02 17:13:58 -0400

    Adding rantalankila segments

--- rantalankilaSegments/experiments/createConfig.m
+++ rantalankilaSegments/experiments/createConfig.m
@@ -2,4 +2,7 @@ function config=createConfig()
 
 config.path.input='/home/gneelima/work/data/datasets/PASCAL2007/testImages/';
 config.path.output='/home/gneelima/work/data/output/objectProposals/rantalankilaSegments/proposals/';
-config.param.imageExt='.jpg';
+
+config.opts.imageExt='.jpg';
+
+
