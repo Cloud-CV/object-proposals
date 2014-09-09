f9189543d1800f39868d1a3b0f38666e997d7e93
Harsh Agrawal <h.agrawal092@gmail.com>
2014-09-08 23:25:54 -0400

    Updating rantalankila

--- rantalankilaSegments/API/calcRaSegments.m
+++ rantalankilaSegments/API/calcRaSegments.m
@@ -26,8 +26,34 @@ function calcRaSegments( configjson )
         
         fprintf('Calculating Rantalankila Segments for %s\n', imname);
         [region_parts, orig_sp] = spagglom(im, opts);
+
+        boxes=zeros(length(region_parts),4);
+        for i=1:length(region_parts)
+                mask=zeros(size(im));
+                region=region_parts{i}
+                for j=1:length(region)
+                        sp=region(i)
+                        pixels=orig_sp{sp};
+                        for k=1:length(pixels)
+                                mask(pixles(k,1),pixels(k,2))=1;
+                        end
+                end
+
+        end
+
+        if(isfield(raconfig.opts,'numProposals'))
+            numProposals=raconfig.opts.numProposals;
+            if(size(boxes,1)>=numProposals)
+                boxes=boxes(1:numProposals,:);
+                            labels=labels(1:numProposals);
+            else
+                fprintf('Only %d proposals were generated for image:%s\n',size(boxes,1),imName);
+            end
+        end
+
+        proposals.boxes=boxes;
         saveFile=[imname '.mat'];
-        save([raconfig.outputLocation saveFile], 'region_parts', 'orig_sp');
+        save([raconfig.outputLocation saveFile], 'proposals');
     end
 
     
