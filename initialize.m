af4c2767bf6129efe265a2540f3ea9f76e0de89e
Harsh Agrawal <h.agrawal092@gmail.com>
2014-09-12 14:36:39 -0400

    adding init script

---
+++ initialize.m
@@ -0,0 +1,56 @@
+    global configjson
+    
+% add current directory as the parent directory
+	parDir = pwd;
+    
+% adding evaluation metrics into path
+    addpath([parDir '/evaluation-metrics']);
+    
+% add jsonlib to path and load the config file
+	addpath([parDir '/jsonlab_1.0beta/jsonlab']);
+	fprintf('Added json encoder/decoder to the path\n');
+    
+    configjson = loadjson([parDir, '/config.json'])
+    configjson.parDir = pwd;
+    
+    addpath(fullfile(pwd, 'utils'));
+    
+	addpath(genpath([parDir '/edgeBoxes']));
+    configjson.edgeBoxes.modelPath = [parDir, '/edgeBoxes/releaseV3/', 'models/forest/modelBsds.mat']
+	configjson.edgeBoxes.params = setEdgeBoxesParamsFromConfig(configjson.edgeBoxes);
+	
+
+    mcg_path = [pwd '/mcg/MCG-Full'];
+	addpath(mcg_path);
+	addpath([pwd '/mcg/API'])
+	%set root_dir for mcg
+	configjson.mcg.root_dir = mcg_root_dir(mcg_path);
+    mcg_install(configjson.mcg.root_dir);
+    %configjson.mcg.db_root_dir = database_root_dir(configjson.mcg);
+
+    endres_path = [pwd '/endres/proposals'];
+    configjson.endres.endrespath = endres_path;
+	addpath(genpath(endres_path));
+
+    addpath(genpath([pwd '/dependencies']));
+	addpath(genpath([pwd '/rantalankilaSegments']));
+	configjson.rantalankila.rapath =   [pwd '/rantalankilaSegments'];
+	configjson.rantalankila.vlfeatpath = [ pwd '/dependencies/vlfeat-0.9.16/' ];
+    
+    addpath(genpath([pwd '/rahtu']));
+	configjson.rahtu.rahtuPath = [pwd '/rahtu/rahtuObjectness'];
+	
+    addpath(genpath([pwd, '/randomizedPrims']));
+	configjson.randomPrim.rpPath = [pwd, '/randomizedPrims/rp-master'];
+    addpath([configjson.randomPrim.rpPath, '/cmex']);
+    
+    addpath(genpath([pwd, '/objectness-release-v2.2']));
+    configjson.objectness.objectnesspath = [pwd, '/objectness-release-v2.2'];
+    
+    addpath(genpath(fullfile(pwd,'selective_search')));
+	configjson.selective_search.params.colorTypes = {'Hsv', 'Lab', 'RGI', 'H', 'Intensity'};
+	configjson.selective_search.params.simFunctionHandles = {@SSSimColourTextureSizeFillOrig, ...
+                      @SSSimTextureSizeFill};
+    fprintf('Initialization finished. All the necessary paths have been set.\n ');
+
+    
