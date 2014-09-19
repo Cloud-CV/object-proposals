556cb3da9543cf037365bba0af40542999f01fdc
Harsh Agrawal <h.agrawal092@gmail.com>
2014-09-18 22:59:47 -0400

    Adding changes Neelima wanted for evaluation metric

--- runObjectProposal.m
+++ runObjectProposal.m
@@ -1,20 +1,31 @@
-function runObjectProposal(name)
-    global configjson;
-    proposalNames = fieldnames(configjson);
-  
-    for i = 1:length(proposalNames)
-        if((strcmp(proposalNames(i), 'imageLocation')==1 || strcmp(proposalNames(i), 'outputLocation')==1 || strcmp(proposalNames(i), 'params')==1))
-            continue;
-        else    
-           fprintf('Coming Here');
-            try
-                funcName = sprintf('calc%s', char(proposalNames(i)));
-                fh = makeHandle(funcName);
-                fh(configjson);
-            catch
-                fprintf('Error running %s\n', funcName);
+function runObjectProposal(varargin)
+    
+    global configjson;    
+    if(~isempty(varargin))
+        proposalName = varargin{1};
+        try
+            funcName = sprintf('calc%s', char(proposalName));
+            fh = makeHandle(funcName);
+            fh(configjson);
+        catch
+            fprintf('Error running %s\n', funcName);
+        end
+    else
+        proposalNames = fieldnames(configjson);
+        for i = 1:length(proposalNames)
+            if((strcmp(proposalNames(i), 'imageLocation')==1 || strcmp(proposalNames(i), 'outputLocation')==1 || strcmp(proposalNames(i), 'params')==1))
+                continue;
+            else    
+                try
+                    funcName = sprintf('calc%s', char(proposalNames(i)));
+                    fh = makeHandle(funcName);
+                    fh(configjson);
+                catch
+                    fprintf('Error running %s\n', funcName);
+                end
             end
         end
+        
     end
 end
 
