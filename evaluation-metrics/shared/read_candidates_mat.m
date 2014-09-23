3fb5ffc3d314b71725be4fdb414b8c4e1e842dee
nchavali <neelima.chavali@gmail.com>
2014-09-22 22:47:54 -0400

    making compute_abo_candidates consistent with current configuration

--- evaluation-metrics/shared/read_candidates_mat.m
+++ evaluation-metrics/shared/read_candidates_mat.m
@@ -1,5 +1,5 @@
 function [boxes, scores, num_candidates] = read_candidates_mat(dirname, img_id)
-  matfile = fullfile(dirname, sprintf('%s.mat', img_id));
+  matfile = fullfile(dirname, sprintf('%s.jpg.mat', img_id));
   % default value
   num_candidates = 10000;
   load(matfile);
