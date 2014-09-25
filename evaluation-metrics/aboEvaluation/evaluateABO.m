5317bb7b061a217104802e5beeddfa3158d19b6d
nchavali <neelima.chavali@gmail.com>
2014-09-25 11:15:19 -0400

    Cleaning up ABO code

--- evaluation-metrics/aboEvaluation/evaluateABO.m
+++ evaluation-metrics/aboEvaluation/evaluateABO.m
@@ -1,4 +1,4 @@
-function evaluateABO( methods, varargin)
+function evaluateABO( config, varargin)
   if(length(varargin)>1)
     output_file_prefix=varargin{1};
     outputLocation = varargin{2};
@@ -12,17 +12,17 @@ function evaluateABO( methods, varargin)
   aboFileName= 'abo_candidates.mat';
   load('aboEvaluation/data/GtPascal2007ObjPRop.mat'); 
   
-  proposalNames = fieldnames(methods)
-  n = length(proposalNames)
+  proposalNames = fieldnames(config);
+  n = length(proposalNames);
   
   for i=1:n
-  	methods.(char(proposalNames(i))).opts.color=(randi(256,1,3)-1)/256;
+  	config.(char(proposalNames(i))).opts.color=(randi(256,1,3)-1)/256;
   end
 
   figure;
   for i = 1:n
-      try
-        data = load(char(fullfile(methods.(char(proposalNames(i))).opts.outputLocation, aboFileName)));
+     % try
+        data = load(char(fullfile(config.(char(proposalNames(i))).opts.outputLocation, aboFileName)));
         num_experiments = numel(data.abo_candidates);
         x = zeros(num_experiments, 1);
         y = zeros(num_experiments, 1);
@@ -32,22 +32,22 @@ function evaluateABO( methods, varargin)
           x(exp_idx) = avg_windows;
           y(exp_idx) = abo;
         end
-        label=methods.(char(proposalNames(i))).opts.name;
+        label=config.(char(proposalNames(i))).opts.name;
         labels{i}=label;
         line_style = '-';
-        if methods.(char(proposalNames(i))).opts.isBaseline
+        if config.(char(proposalNames(i))).opts.isBaseline
           line_style = '--';
         end
-        semilogx(x, y, 'Color', methods.(char(proposalNames(i))).opts.color, 'LineWidth', 1.5, 'LineStyle', line_style);
+        semilogx(x, y, 'Color', config.(char(proposalNames(i))).opts.color, 'LineWidth', 1.5, 'LineStyle', line_style);
         hold on; grid on;
-      catch
-          fprintf('Error evaluating %s\n', (char(proposalNames(i))));
-      end
+     % catch
+      %    fprintf('Error evaluating %s\n', (char(proposalNames(i))));
+     % end
   end
   xlim([10, 10000]);
   ylim([0 1]);
   xlabel('# candidates'); ylabel('average best overlap');
-  legend(labels, 'Location', 'SouthEast');
+  legend(labels{:}, 'Location', 'SouthEast');
   legendshrink(0.5);
   legend boxoff;
   hei = 10;
