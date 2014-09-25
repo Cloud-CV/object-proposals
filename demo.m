039ae0c0b58af6c52481c8dae365910679a2f82d
nchavali <neelima.chavali@gmail.com>
2014-09-25 10:32:22 -0400

    updating demo

--- demo.m
+++ demo.m
@@ -4,4 +4,18 @@ if(~exist(fullfile(pwd, 'demo_result')))
     mkdir(fullfile(pwd, 'demo_result'));
 end
 configjson.outputLocation = fullfile(pwd, 'demo_result');
-calcrandomPrim(configjson);
+
+%%% proposals for al images in the imageLocation
+runObjectProposal('randomPrim');
+
+%%%proposals for one image
+%1)
+proposals=runObjectProposal('randomPrim','demo_img/2007_009084.jpg');
+
+%2)
+im=imread('demo_img/2007_009084.jpg');
+proposals=runObjectProposal('randomPrim',im);
+
+%%% certain number of proposals
+
+proposals=runObjectProposal('randomPrim',im,100);
