e29074b510ad8af58ee0a7de9037f286ae198ae2
Latha Pemula <lpemula@godel.ece.vt.edu>
2014-09-26 14:16:58 -0400

    Added calcrigorForIm function and corresponding changes confirming to the latest API changes.

---
+++ rigor/API/calcrigor.m
@@ -0,0 +1,38 @@
+function calcRigor( config )
+
+%RIGOR Summary of this function goes here
+%   Detailed explanation goes here
+
+rigorconfig = config.rigor;
+param = rigorconfig.params.param_set;
+
+%Check if image location exists or not.
+
+if(~exist(config.imageLocation, 'dir'))
+	fprintf('Image Location does not exist. Please check path once again \n');
+	return;
+end
+
+if(~exist(config.outputLocation, 'dir'))
+	fprintf('Image Location does not exist. Please check path once again \n');
+	return;
+end
+
+%Load All images in a particular folder
+images = dir(config.imageLocation);
+images = regexpi({images.name}, '.*jpg|.*jpeg|.*png|.*bmp', 'match');
+images = [images{:}];
+
+for i=1:length(images)
+    imname = char(images(i));
+    impath = fullfile(config.imageLocation, imname);
+    whos impath
+    im=imread(impath);
+    fprintf('Calculating RIGOR for %s\n', imname);
+    proposals =calcRigorForIm(impath,rigorconfig); 
+    saveFile=[imname '.mat'];
+    save([config.outputLocation saveFile], 'proposals');
+end
+
+end
+
