0a07198c17737c7647c978a91ee034e1a0a396df
Abhishek Das <das.abhshk@gmail.com>
2015-05-26 02:41:01 +0530

    Adds LPO initialization code

--- initialize.m
+++ initialize.m
@@ -1,27 +1,27 @@
 global configjson
-    
+
 % add current directory as the parent directory
 parDir = pwd;
-addpath(genpath(pwd));    
+addpath(genpath(pwd));
 % adding evaluation metrics into path
 addpath(genpath([parDir '/evaluation-metrics']));
-    
+
 % add jsonlib to path and load the config file
 addpath([parDir '/jsonlab_1.0beta/jsonlab']);
 fprintf('Added json encoder/decoder to the path\n');
-   
+
 configjson = loadjson([parDir, '/config.json']);
 configjson.params.parDir = pwd;
-    
+
 addpath(fullfile(pwd, 'utils'));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
 %% configuring of edge boxes
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 addpath(genpath([pwd '/edgeBoxes']));
 configjson.edgeBoxes.modelPath = [parDir, '/edgeBoxes/releaseV3/', 'models/forest/modelBsds.mat'];
 configjson.edgeBoxes.params = setEdgeBoxesParamsFromConfig(configjson.edgeBoxes);
-	
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% configuring MCG  %%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -41,18 +41,18 @@ addpath(genpath(endres_path));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% configuring rantalankila %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 addpath(genpath([pwd '/dependencies']));
 addpath(genpath([pwd '/rantalankilaSegments']));
 configjson.rantalankila.rapath =   [pwd '/rantalankilaSegments'];
 configjson.rantalankila.vlfeatpath = [ pwd '/dependencies/vlfeat-0.9.16/' ];
 spagglom_options;
-configjson.rantalankila.params=opts; 
+configjson.rantalankila.params=opts;
 %%%%%%%%%%%%%%%%%%%%%%%
 %% configuring rahtu %%
 %%%%%%%%%%%%%%%%%%%%%%%
-   
+
 addpath(genpath([pwd '/rahtu']));
 configjson.rahtu.rahtuPath = [pwd '/rahtu/rahtuObjectness'];
 
@@ -86,7 +86,7 @@ configjson.objectness.params=params;
    boost_incl_opt = '';
    boost_lib_opt = '';
    extra_opts = '';
-   
+
    % set directories and options
    if ispc
        % if windows
@@ -95,7 +95,7 @@ configjson.objectness.params=params;
        tbb_libs = fullfile(tbb_dir, 'lib/intel64/vc12');
        boost_dir = 'D:/boost/1.55.0/VC/11.0';
        boost_libs = fullfile(boost_dir, 'stage/lib');
-       
+
        tbb_incl_opt = ['-I', tbb_incl_dir];
        tbb_lib_opt = ['-L', tbb_libs];
        boost_incl_opt = ['-I', boost_dir];
@@ -113,14 +113,14 @@ configjson.objectness.params=params;
    addpath(genpath(rigor_path));
    addpath([pwd '/rigor/API']);
   % make
-  	
+
   %  addpath(genpath([pwd '/dependencies']));
    % find locations of files
    code_root_dir = fullfile(fileparts(which(mfilename)), 'rigor/rigor_src');
    utils_dir = fullfile(code_root_dir, 'utils');
    extern_dir = fullfile(code_root_dir, 'extern_src');
    boykov_dir = fullfile(code_root_dir, 'boykov_maxflow');
- 
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% configuring selective search %%
@@ -129,7 +129,7 @@ configjson.objectness.params=params;
 addpath(genpath(fullfile(pwd,'selective_search')));
 configjson.selective_search.params.colorTypes = {'Hsv', 'Lab', 'RGI', 'H', 'Intensity'};
 configjson.selective_search.params.simFunctionHandles = {@SSSimColourTextureSizeFillOrig,@SSSimTextureSizeFill};
-    
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% configuring Geodesic Object Proposal %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -143,7 +143,28 @@ configjson.gop.gopdatapath=[gopPath '/data'];
     for i = 1:length(proposalNames)
         if((strcmp(proposalNames(i), 'imageLocation')==1 || strcmp(proposalNames(i), 'outputLocation')==1 || strcmp(proposalNames(i), 'params')==1))
             continue;
-        else    
+        else
+            eval(sprintf('configjson.%s.opts.outputLocation = fullfile(configjson.outputLocation,proposalNames(i));',char(proposalNames(i))))
+            eval(sprintf('configjson.%s.opts.name = proposalNames(i);',  char(proposalNames(i)) ))
+            eval(sprintf('configjson.%s.opts.color = (randi(256,1,3)-1)/256;',  char(proposalNames(i))  ))
+
+        end
+    end
+
+%%%%%%%%%%%%%%%%%%%%%
+%% configuring LPO %%
+%%%%%%%%%%%%%%%%%%%%%
+
+lpoPath = [pwd, '/lpo'];
+addpath(genpath(lpoPath));
+configjson.lpo.path=lpoPath;
+
+
+    proposalNames = fieldnames(configjson);
+    for i = 1:length(proposalNames)
+        if((strcmp(proposalNames(i), 'imageLocation')==1 || strcmp(proposalNames(i), 'outputLocation')==1 || strcmp(proposalNames(i), 'params')==1))
+            continue;
+        else
             eval(sprintf('configjson.%s.opts.outputLocation = fullfile(configjson.outputLocation,proposalNames(i));',char(proposalNames(i))))
             eval(sprintf('configjson.%s.opts.name = proposalNames(i);',  char(proposalNames(i)) ))
             eval(sprintf('configjson.%s.opts.color = (randi(256,1,3)-1)/256;',  char(proposalNames(i))  ))
@@ -152,8 +173,9 @@ configjson.gop.gopdatapath=[gopPath '/data'];
     end
 
 
-fprintf('**Initialization finished. All the necessary paths have been set.**\n'); 
+
+fprintf('**Initialization finished. All the necessary paths have been set.**\n');
 
 
-addpath(genpath([pwd, '/dependencies'])); 
+addpath(genpath([pwd, '/dependencies']));
 vl_setup;
